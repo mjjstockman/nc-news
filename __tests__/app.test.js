20ccc883a4335b0f763c50922bf65eb16bd134ee
mjjstockman <mjjstockman@gmail.com>
2024-08-26 12:05:04 +0100

    pass api endpoint test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,23 @@ const app = require('../app');
 const seed = require('../db/seeds/seed');
 const testData = require('../db/data/test-data/');
 const db = require('../db/connection');
+const endpointsData = require('../endpoints.json');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('/api', () => {
+  it('responds with status 200 and documents all endpoints', () => {
+    return request(app)
+      .get('/api')
+      .expect(200)
+      .then(({ body }) => {
+        console.log('Response Body:', body);
+        console.log('Expected Data:', endpointsData);
+        expect(body.endpoints).toEqual(endpointsData);
+      });
+  });
+});
 
 describe('/api/topics', () => {
   it('responds with status 200 and all topics with slug and description properties', () => {
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const { getEndpoints } = require('./controllers/endpoints.controllers');
 const { getTopics } = require('./controllers/topics.controllers');
 const { handleCustomErrors } = require('./errors/handleCustomErrors');
 
+app.get('/api', getEndpoints);
 app.get('/api/topics', getTopics);
 
 app.use((req, res, next) => {
