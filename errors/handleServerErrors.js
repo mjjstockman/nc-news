5f2b6c6999e2a56503b1538383abc01e10dd1dab
Matt Stockman <56722629+mjjstockman@users.noreply.github.com>
2024-08-27 16:26:10 +0100

    Revert "4 core get api articles article"

--- errors/handleServerErrors.js
+++
@@ -1,3 +0,0 @@
-exports.handleServerErrors = (err, req, res, next) => {
-  res.status(500).send({ msg: 'Internal Server Error' });
-};
--- models/topics.models.js
+++
@@ -1,5 +0,0 @@
-const db = require('../db/connection');
-
-exports.fetchTopics = () => {
-  return db.query(`SELECT * FROM topics`).then(({ rows: topics }) => topics);
-};
