b1fa4a7b3b0af2f98ba64ad521d0375abfb1fb5b
mjjstockman <mjjstockman@gmail.com>
2024-08-28 11:40:11 +0100

    change fetchArticles to not include body and add order by

--- models/articles.models.js
+++ models/articles.models.js
@@ -21,11 +21,19 @@ exports.fetchArticles = () => {
   return db
     .query(
       `
-      SELECT articles.*,
-      COUNT(comments.comment_id)::INTEGER AS comment_count
+      SELECT 
+        articles.author, 
+        articles.title, 
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id)::INTEGER AS comment_count
       FROM articles
       LEFT JOIN comments ON comments.article_id = articles.article_id
-      GROUP BY articles.article_id;
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;
       `
     )
     .then(({ rows: articles }) => {
