7ad79976e34bba1bf3d3de8d6a87980023d31158
mjjstockman <mjjstockman@gmail.com>
2024-08-28 15:35:57 +0100

    add and pass test for getting comments by article_id sad path

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,11 +1,22 @@
-const { fetchCommentsByArticleId } = require('../models/comments.models');
+const {
+  fetchCommentsByArticleId,
+  articleExists,
+} = require('../models/comments.models');
 
 exports.getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
 
-  fetchCommentsByArticleId(article_id)
+  articleExists(article_id)
+    .then((exists) => {
+      if (!exists) {
+        return res.status(404).send({ msg: 'Article not found' });
+      }
+      return fetchCommentsByArticleId(article_id);
+    })
     .then((comments) => {
-      res.status(200).send({ comments });
+      if (comments) {
+        res.status(200).send({ comments });
+      }
     })
     .catch(next);
 };
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,5 +1,20 @@
 const db = require('../db/connection');
 
+exports.articleExists = (article_id) => {
+  return db
+    .query(
+      `
+          SELECT *
+          FROM articles
+          WHERE article_id = $1
+        `,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      return rows.length > 0;
+    });
+};
+
 exports.fetchCommentsByArticleId = (article_id) => {
   return db
     .query(
